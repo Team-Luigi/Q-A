8bc4e271be03b2ead2c5f9c30ecae14179a1e105
Anthony Blelloch <anthony.be510@gmail.com>
2022-03-09 14:19:10 -0800

    stress test implementation

--- server/stress.script.js
+++ server/stress.script.js
@@ -2,10 +2,14 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  vus: 100,
-  duration: '10s',
+  vus: 1000,
+  duration: '15s',
 };
+
+const id = Math.floor(Math.random() * 1000011);
+const url = `http://localhost:3000/api/product/questions/${id}`;
+
 export default function () {
-  http.get('http://localhost:3000/api/product/questions/1');
+  http.get(url);
   // sleep(1);
 }
